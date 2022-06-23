d05ffe1ee939a6f69c2e1bdee1eb65d7961dcf2e
austinbhan <austinbhan@outlook.com>
2022-06-23 16:26:41 -0700

    renamed env file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import getCrops from './services/fetch-utlis';
+
+import CropList from './CropList';
 
 function App() {
+  const [crops, setCrops] = useState([]);
+
+  useEffect(() => {
+    async function fetch() {
+      const cropsList = await getCrops(); // Add {from to} when adding pagination
+
+      setCrops(cropsList);
+    }
+    fetch();
+  }, []);
+
   return (
     <div className="App">
       <h1>This is the front page</h1>
+      < CropList 
+        crops={crops}/>
     </div>
   );
 }
---
+++ src/CropList.js
@@ -0,0 +1,15 @@
+import Crop from './Crop';
+
+export default function CropList({ crops }) {
+  return (
+    <div className="crops">
+      {
+        crops.map((crop, i) => {
+          return <Crop
+            {...crop}
+            key={crop.name + i + crop.id} />;
+        })
+      }
+    </div>
+  );
+}
