31ab1bdbaf9c7fcca4fb563af6b0b776289ef4c8
austinbhan <austinbhan@outlook.com>
2022-06-23 15:52:39 -0700

    Crop.js

---
+++ src/Crop.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+// import './App.css'
+
+
+export default function Crop({ name, classification, description }) {
+  return (
+    <div className="crop">
+      <h3>{name}</h3>
+      <p>{classification}</p>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+// Make Link
+// Add Icon
+// Add CSS
