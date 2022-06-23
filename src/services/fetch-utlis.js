6ef88812adf189c2b85f46fe569ab2e6545c0ef7
austinbhan <austinbhan@outlook.com>
2022-06-23 15:44:56 -0700

    Fetch utils finished

--- src/services/fetch-utlis.js
+++ src/services/fetch-utlis.js
@@ -1,6 +1,11 @@
 import { client, checkError } from './client';
 
 export async function getCrops(from = 0, to = 30) {
-  const response = await client.from('Stardew_Valley_Crops_Trees').select('*');
+  const response = await client.from('Stardew_Valley_Crops_Trees').select().range(from, to);
+  return checkError(response);
+}
+
+export async function getSingleCrop(id) {
+  const response = await client.from('Stardew_Valley_Crops_Trees').select().match({ id }).single();
   return checkError(response);
 }
