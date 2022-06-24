0a23dbe84956ae3b03458fab18862bc706f0d565
austinbhan <austinbhan@outlook.com>
2022-06-23 23:01:55 -0700

    CSS Centering Cards

--- src/CropPage.js
+++ src/CropPage.js
@@ -2,11 +2,12 @@ import { useState, useEffect } from 'react';
 import React from 'react';
 import { getCrops } from './services/fetch-utlis';
 import CropList from './CropList';
+import './App.css';
 
 export default function CropPage() {
   const [crops, setCrops] = useState([]);
   const [page, setPage] = useState(1);
-  const perPage = 19;
+  const perPage = 5;
   const lastPage = Math.floor(crops / perPage);
 
   useEffect(() => {
@@ -31,13 +32,16 @@ export default function CropPage() {
 
   return (
     <>
-      <h2>Current Page: {page}</h2>
-      <div className="button">
-        <button disabled={page <= 0} onClick={() => setPage(page - 1)}>Previous</button>
-        <button disabled={page >= lastPage} onClick={() => setPage(page + 1)}>Next</button>
+      <div className="crop-card">
+        <h2>Current Page: {page}</h2>
+        <div className="button">
+          <button disabled={page <= 0} onClick={() => setPage(page - 1)}>Previous</button>
+          <button disabled={page >= lastPage} onClick={() => setPage(page + 1)}>Next</button>
+        </div>
+        <CropList
+          crops={crops} />
+        
       </div>
-      <CropList
-        crops={crops} />
     </>
   );
 }
