9198799d28491a99b150f5d4fcc6c05fef158018
austinbhan <austinbhan@outlook.com>
2022-06-23 22:39:02 -0700

    Page Scrolling Works

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 
 import { BrowserRouter as Router,
-// Add Switch when Ready
+  Switch,
   Route,
 } from 'react-router-dom';
 
@@ -16,16 +16,15 @@ function App() {
     <>
       <Router>
         <div className="App">
-          {/* Need to Add Switch */}
-          <h1>This is the front page</h1>
-          <Route exact path = '/'> 
-            <CropPage />
-          </Route>
-          <Route exact path ='/crops/:id'>
-            <CropDetail  // Details go Here
-            />
-          </Route>
-          {/* Need to Add Switch */}
+          <Switch>
+            <Route exact path = '/'> 
+              <CropPage />
+            </Route>
+            <Route exact path ='/crops/:id'>
+              <CropDetail  // Details go Here
+              />
+            </Route>
+          </Switch>
         </div>
       </Router>
     </>
--- src/CropPage.js
+++ src/CropPage.js
@@ -5,18 +5,37 @@ import CropList from './CropList';
 
 export default function CropPage() {
   const [crops, setCrops] = useState([]);
+  const [page, setPage] = useState(1);
+  const perPage = 19;
+  const lastPage = Math.floor(crops / perPage);
 
   useEffect(() => {
     async function fetch() {
       const cropsList = await getCrops();
-
       setCrops(cropsList);
     }
     fetch();
   }, []);
 
+  useEffect(() => {
+    async function fetch() {
+      const from = page * perPage - perPage;
+      const to = page * perPage;
+      const crops = await getCrops(from, to);
+
+      setCrops(crops);
+    }
+
+    fetch();
+  }, [page]);
+
   return (
     <>
+      <h2>Current Page: {page}</h2>
+      <div className="button">
+        <button disabled={page <= 0} onClick={() => setPage(page - 1)}>Previous</button>
+        <button disabled={page >= lastPage} onClick={() => setPage(page + 1)}>Next</button>
+      </div>
       <CropList
         crops={crops} />
     </>
